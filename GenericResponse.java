416e50e158c1e5ad985f4035594d59ec80faa0a5
BABLESH RAJPOOT <43149655+bablesh-rajpoot@users.noreply.github.com>
2020-04-10 18:14:00 +0530

    This is model

---
+++ GenericResponse.java
@@ -0,0 +1,35 @@
+package com.example.demo.model;
+
+public class GenericResponse 
+{
+
+	private Integer status;
+	private Object data;
+	private String error;
+	private String message;
+
+	public Integer getStatus() {
+		return status;
+	}
+	public void setStatus(Integer status) {
+		this.status = status;
+	}
+	public Object getData() {
+		return data;
+	}
+	public void setData(Object data) {
+		this.data = data;
+	}
+	public String getError() {
+		return error;
+	}
+	public void setError(String error) {
+		this.error = error;
+	}
+	public String getMessage() {
+		return message;
+	}
+	public void setMessage(String message) {
+		this.message = message;
+	}
+}
